26033087fb6297718a8d7d1c817ccb2e7f71d090
Kent <kent7782@gmail.com>
2025-04-23 23:29:48 +0200

    chore: test setting up profolio starter

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,43 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  output: "export",
   basePath: process.env.PAGES_BASE_PATH,
+  images: {
+    unoptimized: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/rss.xml",
+        destination: "/feed/rss.xml",
+      },
+      {
+        source: "/atom.xml",
+        destination: "/feed/atom.xml",
+      },
+      {
+        source: "/feed.json",
+        destination: "/feed/feed.json",
+      },
+      {
+        source: "/rss",
+        destination: "/feed/rss.xml",
+      },
+      {
+        source: "/feed",
+        destination: "/feed/rss.xml",
+      },
+      {
+        source: "/atom",
+        destination: "/feed/atom.xml",
+      },
+      {
+        source: "/json",
+        destination: "/feed/feed.json",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
